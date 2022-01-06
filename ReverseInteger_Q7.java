ecc1d0c60a3428220d3e64cfc06c694f2d0f270f
Adithya Sundararajan Iyer <72000759+Adithya-Iyer@users.noreply.github.com>
2022-01-05 19:42:57 -0600

    Create ReverseInteger_Q7.java

---
+++ ReverseInteger_Q7.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int reverse(int x) {
+        //x = 1534236469;
+        //System.out.println((int)9646324351);
+        long r=0;
+        while(x!=0) {
+            r = r*10 + x%10;
+            x/=10;
+            System.out.println(r);
+        }
+        if ((-2147483648 <= r) && (r <= 2147483647)) {
+            return (int)r;
+        } 
+        else {
+            return 0;
+        }
+    }
+}
