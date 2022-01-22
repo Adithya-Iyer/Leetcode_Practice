012f6a5aa539523822c406370e031b5840517fa8
Adithya Sundararajan Iyer <72000759+Adithya-Iyer@users.noreply.github.com>
2022-01-22 10:59:21 -0600

    Time: 636 ms (40.00%), Space: 15.6 MB (40.00%) - LeetHub

--- 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.py
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.py
@@ -1,6 +1,6 @@
 class Solution:
     def numberOfWays(self, corridor: str) -> int:
-        seatCounter = [0]
+        """seatCounter = [0]
         plantCounter = []
         s = x = y = 0
         n = len(corridor)
@@ -15,5 +15,20 @@ class Solution:
                 if (seatCounter[s]==2):
                     x = i
         if(seatCounter[s]<2):
+            return 0
+        return (math.prod(plantCounter)%(10**9 + 7))"""
+        plantCounter = []
+        seat = x = y = 0
+        n = len(corridor)
+        for i, c in enumerate(corridor):
+            if (c=='S'):
+                if (seat==2):
+                    seat = 0
+                    y = i
+                    plantCounter.append(y-x)
+                seat+=1
+                if (seat==2):
+                    x = i
+        if(seat<2):
             return 0
         return (math.prod(plantCounter)%(10**9 + 7))
