28aeff133114ef99b17d4bf87986b24805573223
Adithya Sundararajan Iyer <72000759+Adithya-Iyer@users.noreply.github.com>
2022-01-26 23:54:53 -0600

    Update 1305-all-elements-in-two-binary-search-trees.py

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.py
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.py
@@ -7,6 +7,8 @@
 class Solution:
     def getAllElements(self, root1: TreeNode, root2: TreeNode) -> List[int]:
         
+        #DISCLAIMER: NOT MY ORIGINAL CODE
+        
         def dm(root):
             if not root:
                 return []
