a40989d3c8194d5b88e5c677bb6b5a3817cb9fa0
Adithya Sundararajan Iyer <72000759+Adithya-Iyer@users.noreply.github.com>
2022-02-15 02:50:43 -0600

    Time: 216 ms (41.38%), Space: 15.3 MB (77.92%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -1,9 +1,17 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        #O(nlogn) approach
-        nums.sort()
         n = len(nums)
+        #Time=O(nlogn) and Space=O(1) approach
+        """nums.sort()
         for i in range(n):
             if(i!=nums[i]):
+                return i
+        return n"""
+        #Time=O(n) and Space=O(n) approach
+        arr = [None for i in range(n+1)]
+        for x in nums:
+            arr[x] = 1
+        for i in range(n+1):
+            if (arr[i]==None):
                 return i
         return n
