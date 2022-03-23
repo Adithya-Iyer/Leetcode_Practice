f72f5f3b3625d48c8621304108c10df452180343
Adithya Sundararajan Iyer <72000759+Adithya-Iyer@users.noreply.github.com>
2022-03-23 03:46:28 -0500

    Time: 593 ms (35.06%), Space: 15.8 MB (97.41%) - LeetHub

---
+++ 888-fair-candy-swap/888-fair-candy-swap.py
@@ -0,0 +1,20 @@
+class Solution:
+    def fairCandySwap(self, aliceSizes: List[int], bobSizes: List[int]) -> List[int]:
+        aSum = sum(aliceSizes)
+        bSum = sum(bobSizes)
+        # asL = sorted(list(set(aliceSizes)))
+        # bsL = sorted(list(set(bobSizes)))
+        asL = sorted(aliceSizes)
+        bsL = sorted(bobSizes)
+        an, bn = len(asL), len(bsL)
+        ap = bp = 0
+        while(ap<an and bp<bn):
+            d = asL[ap]-bsL[bp]
+            at, bt = aSum-d, bSum+d
+            if (at==bt):
+                break
+            elif (at>bt):
+                ap+=1
+            elif (at<bt):
+                bp+=1
+        return [asL[ap],bsL[bp]]
