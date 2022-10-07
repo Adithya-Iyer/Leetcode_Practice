610dabd2888c2f6f8ba509e7ec3c3084baf77a9b
Adithya Sundararajan Iyer <72000759+Adithya-Iyer@users.noreply.github.com>
2022-10-06 19:31:02 -0500

    Time: 64 ms (37.85%), Space: 13.8 MB (95.92%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.py
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -5,6 +5,14 @@ class Solution:
             hashmap[n] = hashmap.get(n,0) + 1
         vals = list(hashmap.values())
         gp = 0
+        # Approach 1
+        # for v in vals:
+        #     gp += v*(v-1)//2
+        # Approach 2
+        highest = max(vals)
+        pairs = [0,0]
+        for i in range(1,highest):
+            pairs.append(pairs[i]+i)
         for v in vals:
-            gp += v*(v-1)//2
+            gp += pairs[v]
         return gp
