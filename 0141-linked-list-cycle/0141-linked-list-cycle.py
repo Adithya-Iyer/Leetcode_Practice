5465e2863e1410991d597bde217d24cc0a01f4d0
Adithya Sundararajan Iyer <72000759+Adithya-Iyer@users.noreply.github.com>
2022-10-26 11:15:42 -0500

    Time: 123 ms (29.98%), Space: 17.9 MB (19.19%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -7,10 +7,18 @@
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         # O(1) space complexity approach
-        i = 0
-        while(head and i<10001):
+        # i = 0
+        # while(head and i<10001):
+        #     head = head.next
+        #     i += 1
+        # if i==10001:
+        #     return True
+        # return False
+        # O(n) space complexity approach
+        hashset = set()
+        while(head and head not in hashset):
+            hashset.add(head)
             head = head.next
-            i += 1
-        if i==10001:
+        if head:
             return True
         return False
