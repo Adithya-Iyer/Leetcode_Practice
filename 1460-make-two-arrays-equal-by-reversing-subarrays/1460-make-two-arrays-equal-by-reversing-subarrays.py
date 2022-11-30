19dd6265e84e06afbd8654b6db6110576d94fcec
Adithya Sundararajan Iyer <72000759+Adithya-Iyer@users.noreply.github.com>
2022-11-30 17:31:07 -0600

    Time: 242 ms (7.88%), Space: 14.1 MB (41.29%) - LeetHub

---
+++ 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.py
@@ -0,0 +1,8 @@
+class Solution:
+    def canBeEqual(self, target: List[int], arr: List[int]) -> bool:
+        h1, h2 = {}, {}
+        for a in arr:
+            h1[a] = h1.get(a,0) + 1
+        for t in target:
+            h2[t] = h2.get(t,0) + 1
+        return h1==h2
